38c2bbe9e07c09c7ef3ad73648111e29f4bd6e56
Isarel Getu <israelgetu101@gmail.com>
2022-04-26 00:42:12 -0500

    40% done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import ExpenseItems from "./components/ExpenseItem";
 
 function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <p>
-      Cleanedup React App
-    </p>
+    <div>
+      <h1> Expense Items!!</h1>
+      <ExpenseItems
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+      ></ExpenseItems>
+    </div>
   );
 }
 
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import "./ExpenseItem.css";
+
+function ExpenseItems(props) {
+  // props = { amount, date, title };
+  return (
+    <div className="expense-item">
+      <div>{props.date}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItems;
